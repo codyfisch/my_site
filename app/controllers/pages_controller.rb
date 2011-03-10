f33f1fae359d396854b9cca1f361094ad471a8e6
Cody Fischel <cody.fischel@gmail.com>
2011-03-10 14:51:43 -0500

    Ending the layout switching to blog model formating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @big_title = "welcome."
   end
 
   def contact
     @title = "Contact"
+    @big_title = "contact."
   end
 
   def about
     @title = "About"
+    @big_title = "about."
   end
 
 end
