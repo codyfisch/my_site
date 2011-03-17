caef552e03c4271dcde3d9d5a1acbea1a00038e6
Cody Fischel <cody.fischel@gmail.com>
2011-03-17 16:29:21 -0400

    End of action-pack. There is something wrong with the delete links and the look of the site. It would appear to be my fault

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  before_filter :load_article, :except => :destroy
+  before_filter :authenticate, :only => :destroy
+
+  def create
+    @comment = @article.comments.new(params[:comment])
+    if @comment.save
+      redirect_to @article, :notice => 'Thanks for your comment'
+    else
+      redirect_to @article, :alert => 'Unable to add comment'
+    end
+  end
+  
+  def destroy
+    @article = current_user.articles.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @article, :notice => 'Comment deleted'
+  end
+  
+  private
+    def load_article
+#      @article = current_user.articles.find(params[:article_id])
+      @article = Article.find(params[:article_id])
+    end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  # Creates a submit button with the given name with a cancel link
+  # Accepts two arguments: Form object and the cancel link name
+  def submit_or_cancel(form, name='Cancel')
+    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  end
+
+
 end
